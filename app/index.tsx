36b739109d203a73bda5ecf4f82d8619042edf38
Yash <darbaryash44@gmail.com>
2025-11-09 01:31:36 +0530

    project setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+import "./globals.css";
+
+export default function Index() {
+  return (
+    <View className="flex-1 justify-center items-center text-red-500"
+    >
+      <Text className="text-blue-500">app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
