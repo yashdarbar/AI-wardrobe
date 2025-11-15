6a8dec483ee6d93d91ad9ce8a6cdad31fbdc0f0a
Yash <darbaryash44@gmail.com>
2025-11-15 22:44:11 +0530

    added some dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,30 @@
 import { Text, View, TouchableOpacity } from "react-native";
 import { Link } from "expo-router";
 import "./globals.css";
+import { Session } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+import { supabase } from "@/lib/supabase";
+import Account from "@/components/Account";
+import Auth from "@/components/Auth";
 
 export default function Index() {
+
+    const [session, setSession] = useState<Session | null>(null);
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
+
   return (
       <View className="flex-1 items-center justify-center bg-white">
           <Text className="text-2xl font-bold mb-4">
               👕 AI Wardrobe Assistant
           </Text>
-          <Text className="text-gray-500 mb-8">Organize. Style. Try-on.</Text>
+          {/* <Text className="text-gray-500 mb-8">Organize. Style. Try-on.</Text>
 
           <Link href="/(tabs)" asChild>
               <TouchableOpacity className="bg-black px-6 py-3 rounded-2xl">
@@ -16,7 +32,12 @@ export default function Index() {
                       Start Exploring
                   </Text>
               </TouchableOpacity>
-          </Link>
+          </Link> */}
+          {session && session.user ? (
+              <Account key={session.user.id} session={session} />
+          ) : (
+              <Auth />
+          )}
       </View>
   );
 }
