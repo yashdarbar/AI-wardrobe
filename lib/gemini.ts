f01faf5689632fa8f75ed2760126b3ab93d5a0e2
Yash <darbaryash44@gmail.com>
2025-12-13 22:13:01 +0530

    remove the api key

--- lib/gemini.ts
+++ lib/gemini.ts
@@ -4,7 +4,6 @@ import { GoogleGenAI } from "@google/genai";
 import { encode } from "base64-arraybuffer";
 // import * as FileSystem from "expo-file-system";
 
-const API_KEY = "AIzaSyA1KCpMErEGPuSJ5rLN41LkxxRsq6gRGDM";
 // const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(API_KEY || "");
 const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
